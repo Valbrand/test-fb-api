bcb97775f15516d0d52a182b6b2d0c907f59e834
Valbrand <daniel@dvalbrand.com>
2015-03-17 21:14:55 -0300

    FB API app access token is being obtained

--- app/controllers/fbfeed_controller.rb
+++ app/controllers/fbfeed_controller.rb
@@ -3,5 +3,32 @@ class FbfeedController < ApplicationController
   end
 
   def addsubscription
+  	require "rest-client"
+  	require "rack/utils"
+
+  	if $redis.get('apptoken').nil?
+  		response = RestClient.get('https://graph.facebook.com/oauth/access_token', {
+  			:params => {
+  				:client_id => Rails.application.secrets.fb_app_id,
+  				:client_secret => Rails.application.secrets.fb_app_secret,
+  				:grant_type => :client_credentials
+  			}
+  		})
+
+  		if response.is_a? String
+  			parsed_response = Rack::Utils.parse_nested_query(response)
+
+  			if parsed_response.key? "access_token"
+  				$redis.set("apptoken", parsed_response["access_token"])
+  			else
+  				raise "Unable to obtain app access token."
+  			end
+  		end
+  	end
+
+  	render 'index'
+
+  	#subscribe to app
+  	#submit to redis
   end
 end
